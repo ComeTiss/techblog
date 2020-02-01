33a85c4d03faa2edebc82cd48f3c9d1a29ff2f72
Come Tisserand <come_tisserand@Comes-MacBook-Pro.local>
2020-02-02 00:24:47 +0100

    changed tree structure

--- startapp.sh
+++ startapp.sh
@@ -1,12 +1,11 @@
 echo
-echo
 echo "STEP 1 - Removing volumes..."
 docker-compose down -v
 echo "DONE!"
 echo
 echo
 echo "STEP 2 - Deleting Images"
-docker rmi techblog_    reactapp
+docker rmi techblog_  reactapp
 echo
 echo
 echo "STEP 3 - Starting docker-compose..."
