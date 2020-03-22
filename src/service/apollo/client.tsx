b78bf35b37f2c003436f1609bcb90649324ab513
ComeTiss <cometisserand@rocketmail.com>
2020-03-22 10:10:20 +0000

    [minor] remove console.log

--- src/service/apollo/client.tsx
+++ src/service/apollo/client.tsx
@@ -3,7 +3,7 @@ import ApolloClient from "apollo-boost";
 const URI_DEV = "http://localhost:9090/posts";
 const URI_PROD = "https://tech-website-backend.herokuapp.com/posts";
 const ENV = process.env.NODE_ENV;
-
+console.log(process.env.NODE_ENV);
 export const client = new ApolloClient({
   uri: ENV !== "production" ? URI_DEV : URI_PROD
 });
