28b080c131fa0aa0d0ddc33a00e5365cf37115c5
ComeTiss <cometisserand@rocketmail.com>
2020-03-26 17:48:14 +0000

    [apollo][minor] changed client URI to /graphql

--- src/service/apollo/client.tsx
+++ src/service/apollo/client.tsx
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
-const URI_DEV = "http://localhost:9090/posts";
-const URI_PROD = "https://tech-website-backend.herokuapp.com/posts";
+const URI_DEV = "http://localhost:9090/graphql";
+const URI_PROD = "https://tech-website-backend.herokuapp.com/graphql";
 const target_uri = process.env.NODE_ENV === "production" ? URI_PROD : URI_DEV;
 
 export const client = new ApolloClient({
